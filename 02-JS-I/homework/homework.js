536ca62418653014ccaefe2a1862ca6bfd1c3c7c
Johan Stiven Castrillon Lopez <estivenjc04@hotmail.com>
2022-10-03 07:23:27 -0500

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 1.8993;
 
 // Crea una variable booleana:
 const nuevoBool = null;
